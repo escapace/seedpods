e2c2f6087d42071dde51c54dd370ae15334670b5
Mark Martirosian <hello@markmartirosian.com>
2023-07-03 21:48:15 +0000

    fix: remove lodash-es dependency

---
+++ src/utilities/is-equal.ts
@@ -0,0 +1,68 @@
+export type AnyObject =
+  | null
+  | undefined
+  | Record<number | string | symbol, unknown>
+
+// export const isUndefined = (value: unknown): value is undefined =>
+//   typeof value === 'undefined'
+// export const isFunction = (value: unknown): value is Function =>
+//   typeof value === 'function'
+// export const isNull = (value: unknown): value is null => value === 'null'
+
+export const isObject = (value: unknown): value is AnyObject =>
+  typeof value === 'object'
+
+function equalArray(left: unknown[], right: unknown[]) {
+  const { length } = left
+
+  if (length !== right.length) {
+    return false
+  }
+
+  for (let index = length; index-- !== 0; ) {
+    if (!isEqual(left[index], right[index])) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export function isEqual(left: unknown, right: unknown) {
+  if (left === right) {
+    return true
+  }
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (left && isObject(left) && right && isObject(right)) {
+    if (left.constructor !== right.constructor) {
+      return false
+    }
+
+    if (Array.isArray(left) && Array.isArray(right)) {
+      return equalArray(left, right)
+    }
+
+    if (left.valueOf !== Object.prototype.valueOf) {
+      return left.valueOf() === right.valueOf()
+    }
+
+    const leftKeys = Object.keys(left)
+
+    for (let index = leftKeys.length; index-- !== 0; ) {
+      const key = leftKeys[index]
+
+      if (!isEqual(left[key], right[key])) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  if (Number.isNaN(left) && Number.isNaN(right)) {
+    return true
+  }
+
+  return left === right
+}
