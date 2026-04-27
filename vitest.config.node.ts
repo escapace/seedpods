188d2d5165b32b81a8e62ed5d7426e8981f68f86
yyxi <178086254+yyxi@users.noreply.github.com>
2026-04-27 09:55:28 +0000

    chore: upkeep

--- vitest.config.node.ts
+++ vitest.config.node.ts
@@ -7,9 +7,16 @@ export default mergeConfig(
   configShared,
   defineConfig({
     define: {
-      ...builds.node.define,
+      ...['node', 'neutral', 'browser']
+        .map(
+          (target) =>
+            (Reflect.get(builds, target) as { define?: Record<string, string> } | undefined)
+              ?.define,
+        )
+        .find((value) => value !== undefined),
       __ENVIRONMENT__: JSON.stringify('development'),
       __VERSION__: JSON.stringify(version),
+      __VITEST_PROJECT__: JSON.stringify('node'),
     },
     test: {
       environment: 'node',
